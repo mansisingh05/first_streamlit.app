da587458f22b7f9a33f2163317127aa9c7bda6d6
mansisingh05 <77797701+mansisingh05@users.noreply.github.com>
2023-05-15 23:58:15 +0530

    Update streamlit_app.py
    chhose fruits by cust

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ my_fruit_list=my_fruit_list.set_index('Fruit')
 
 
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberry'])
 
 streamlit.dataframe(my_fruit_list)  
 
