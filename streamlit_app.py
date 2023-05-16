732b98734688f5117a251433d6bd6eb37cd122a1
mansisingh05 <77797701+mansisingh05@users.noreply.github.com>
2023-05-16 17:33:18 +0530

    Update streamlit_app.py
    add pakage response

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)  
 
 import requests 
-fruityvice_response= request.get("http://fruityvice.com/api/fruit/watermeleon')
+fruityvice_response= request.get("http://fruityvice.com/api/fruit/watermeleon")
 streamlit.text?(fruityvice_response)                                 
 
 
