a7f63d55206e774423fc49c8ab0ba47f790230ea
mansisingh05 <77797701+mansisingh05@users.noreply.github.com>
2023-05-16 17:25:58 +0530

    Update streamlit_app.py
    python package request

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,11 +15,13 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 
 my_fruit_list=my_fruit_list.set_index('Fruit')
 
-
-
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)  
+
+import requests 
+fruityvice_response= request.get("http://fruityvice.com/api/fruit/watermeleon')
+streamlit.text?(fruityvice_response)                                 
 
 
 
