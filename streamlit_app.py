45cf208eb7df9f68b4ef41c998e596b308267657
mansisingh05 <77797701+mansisingh05@users.noreply.github.com>
2023-05-19 10:05:36 +0530

    Update streamlit_app.py
    tt

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,51 +22,7 @@ my_fruit_list=my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)  
-def get_fruityvice_data(this_fruit_choice):
-    fruityvice_response= requests.get("http://fruityvice.com/api/fruit/" + this_fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    return fruityvice_normalized
 
-# new section to display
-streamlit.header('fruityvice fruit advice !')
-try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about ? ')
-    if not fruit_choice:
-        streamlit.error("please select a fruit to get a information ")
-    else:
-       back_from_function = get_fruityvice_data(fruit_choice)
-       streamlit.dataframe(back_from_function) 
-except URLError as e:
-    streamlit.error()
-   
-
-streamlit.header("the fruit load list contains:")
-#snowflake related function
-def get_fruit_load_list():
-     with my_cnx.cursor() as my_cur:
-          my_cur.execute(" select * from fruit_load_list ")
-          return my_cur.fetchall()
-#add a button 
-
-if streamlit.button(' get  fruit load list '):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    my_data_rows = get_fruit_load_list()
-    my_cnx.close()
-    streamlit.dataframe(my_data_rows)
-streamlit.stop()
-def insert_row_snowflake(new_fruit):
-     with my_cnx.cursor() as my_cur: 
-           my_cur.execute("insert into fruit_load_list values('from streamlit')")
-           return "thanks for adding " + new_fruit
-
-add_my_fruit= streamlit.text_input('what fruit would you like to add ?')
-if streamlit.button('add a fruit to the list'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    back_from_function = insert_row_snowflake(add_my_fruit)
-    my_cnx.close()
-    streamlit.text(back_from_function) 
-    
-    
 
 
 
