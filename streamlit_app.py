94a1ddd025db95017a1f5c291f89782f95ff1210
mansisingh05 <77797701+mansisingh05@users.noreply.github.com>
2023-05-19 10:11:22 +0530

    Update streamlit_app.py
    aaa

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,8 @@ my_fruit_list=my_fruit_list.set_index('Fruit')
 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-streamlit.dataframe(fruits_to_show)  
- def get_fruityvice_data(this_fruit_choice):
+streamlit.dataframe(fruits_to_show)
+def get_fruityvice_data(this_fruit_choice):
     fruityvice_response= requests.get("http://fruityvice.com/api/fruit/" + this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
@@ -38,6 +38,8 @@ try:
        streamlit.dataframe(back_from_function) 
 except URLError as e:
     streamlit.error()
+  
+
 
 
 
